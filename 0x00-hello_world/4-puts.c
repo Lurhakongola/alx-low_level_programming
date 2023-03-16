12ad127a454b698e08a76f63a65acc707c0f521b
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-03-16 16:17:28 -0700

    c compiler

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#!/bin/bash <stdio.h>
+/**
+ *main * main entry point
+ *return : always 0 ( success)
+int main(void)
+{
+	puts(*\*Programming is like building a multilingual puzzle*);
+	return (0);
+}
