157a035d0a2f1a3ddc44b12ece4c6ad3e0410e96
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-03-17 13:35:12 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
- *
- * return: always 0 (success)
- */
+   /**
+ * main -Entry point 
+    *
+    * Return: Always 0 (success)
+   */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+print("with proper programmar , but the outcome is a piece of art, \N");
 	return (0);
 }
-
