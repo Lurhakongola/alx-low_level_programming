bdec0146e3975fe77628725e9929d7cbad033d40
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-03-27 16:42:14 -0700

    Reset N to 98

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#iclude <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to pront
+ *
+ * Return: on success 1.
+ * on  errer, -1 is returned, and erno is set appropretely.
+ */
+int _putchar(char c)
+{
+	return(write(1,&c, 1));
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MIAIN_H
+int __putchar(char c);
+void reset_to_98(int *n);
+
+#endif
