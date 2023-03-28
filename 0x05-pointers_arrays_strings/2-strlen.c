c4154b1a218c2b7015d4586010db3189cb501741
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-03-28 02:41:11 -0700

    string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,13 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * _strlen - Returns the length of a string
  * @s: string to count
  *
- * Return: string length
+* Return: string length
  */
 
 int _strlen(char *s)
 {
-	int c = 0;
-
-	for (; *s != '\0' s++)
-	{
-		c++;
-	}
-
-	return (c);
+	return (strlen(s));
 }
