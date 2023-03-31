9c92e14ee618500f08e455fa10c0ebddaed6b768
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-03-31 09:17:04 -0700

    function that checks for uppercase character.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,19 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
- * _isdigit - check if numbers are 0 - 9
- * @c: char to be checked
+ * main - check the code.
  *
- * Return: 0 or 1
+ * Return: Always 0.
  */
-
-int _isdigit(int c)
+int main(void)
 {
-if (c >= '0' && c <= '9')
-return (1);
-else
-return (0)
-}
+char c;
 
+c = 'A';
+printf("%c: %d\n", c, _isupper(c));
+c = 'a';
+printf("%c: %d\n", c, _isupper(c));
+return (0);
+}
 
