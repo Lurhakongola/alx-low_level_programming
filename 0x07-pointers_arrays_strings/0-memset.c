5cfab07fdaa6a354bce6dae4e73704269eec20eb
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-04-04 10:19:14 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+include "mainh"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: niumber of times to copy b
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+
+	for(i=o; i<n; i++)
+	{
+		s[i] = b;
+	}
+	return (s)
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+Even more pointers, arrays and strings in C
+
+0-memset.c is the a function that fills memory with a constant byte
+1-memcpy.c is a function that copies memory area.
+ 2-strchr.c 2-strchr.ca function that locates a character in a string.
