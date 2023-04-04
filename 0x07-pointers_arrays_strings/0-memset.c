f6b284c0213769992fe98561fd3374f53058316b
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-04-04 12:36:48 -0700

    memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,16 +2,16 @@ include "mainh"
 /**
  * _memset - fills memory with a constant byte
  * @s: memory area to be filled
- * @b: char to copy
- * @n: niumber of times to copy b
+ * @b: the desire value
+ * @n: number of bytes to be changed
  *
+ * Return: changed array with new value for n bytes
  */
-
 char *_memset(char *s, char b, unsigned int n);
 {
 	unsigned int i;
 
-	for(i=o; i<n; i++)
+	for (i = o; i < n; i++)
 	{
 		s[i] = b;
 	}
