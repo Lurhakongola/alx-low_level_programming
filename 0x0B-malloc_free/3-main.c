51fab656d21e6e623e0eea0f606c0d26ece4ce70
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-04-12 17:32:52 -0700

    function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -28,18 +28,3 @@ void print_grid(int **grid, int width, int height)
         h++;
     }   
 }
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int **grid;
-
-    grid = alloc_grid(6, 4);
-    if (grid == NULL)
-    {
-        return (1);
-    }
