e054d6423877467cdf67cce9fe24873ac344f65a
miahjerim <miahjerim@gmail.com>
2023-04-28 17:17:41 -0700

    function that adds number to thr beggining of linklist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,17 +9,19 @@
 
 size_t print_list(const list_t *h)
 
-{
-	size_t x = 0;
-
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u]%s\n", h->len, h->str);
-		x++;
-		h = h->next;
-	}
-		return (x);
 }
+
+size_t n = 0;
+
+while (h)
+{
+if (!h->str)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+n++;
+}
+return (n);
+}
+
