abe40e6540002b96366ce3f8a7d33fcda0ea04c4
miahjerim <miahjerim@gmail.com>
2023-04-28 22:37:51 -0700

    function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,20 +8,17 @@
  */
 
 size_t print_list(const list_t *h)
-
-}
-
-size_t n = 0;
-
-while (h)
 {
-if (!h->str)
-printf("[0] (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
-h = h->next;
-n++;
-}
-return (n);
-}
+	size_t n = 0;
 
+	while (h)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,18 +9,21 @@
  * @head: head of a list_t list
  * Return: return the address of the new element, or NULL if it fails
  */
-list_t *add_node(list_t **head, const char *str);
-{
-list_t *(new);
-unsigned int len = 0;
-while (str[len])
-new = malloc(sizeof(list_t));
-if (!new)
-return (NULL);
-new->str = strdup(str);
 
-new->len = _strlen(str);
-new->next = *head;
-*head = new;
-return (head);
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int count = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new  == NULL)
+		return (NULL);
+	while (str[count])
+		count++;
+
+	new->len = count;
+	new->str = strdup(str);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
