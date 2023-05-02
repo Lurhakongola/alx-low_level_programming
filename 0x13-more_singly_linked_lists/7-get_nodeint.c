32e8f0cbe62757d4a13abb6804f1092fc529d660
miahjerim <miahjerim@gmail.com>
2023-05-01 23:36:57 -0700

    function that returns the index number of linked list nodes

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: head of nodes in the list.
+ * @index: index of the nod.
+ * Return: retun NULL if node does not exist.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp  = head;
+       unsigned int j= 0; /* the index of the node we're current looking at */
+
+	while (temp != NULL && j< index)
+	{
+		temp = temp->next;
+	j++;
+	}
+	if(temp == NULL)
+		return NULL;
+
+return (temp);
+}
