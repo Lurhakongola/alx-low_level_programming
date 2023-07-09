a9564ee3267a60555ce12c570a0432e5f37fd705
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-07-09 15:04:11 -0700

    binary changes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec_val = 0;
+	int i;
+	unsigned int convert_num = 0;
 
 	if (b == NULL)
-	{
 		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] !=  '1')
+			return (0);
+		convert_num <<= 1;
+		convert_num += b[i] - '0';
 	}
 
-	int length = strlen(b);
-	for (int i = 0; i < length; i++)
-		;
-	{
-		dec_val *= 2;
-		dec_val += (b[i] - '0');
-				}
-				return (dec_val);
-				}
+	return (convert_num);
+}
