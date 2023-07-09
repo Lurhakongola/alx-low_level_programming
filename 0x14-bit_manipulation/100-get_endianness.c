87435d3fc127f0c93b004e7cbf3b8391402d438a
lurhakongola <alfredbalazirelurhakongola@gmail.com>
2023-07-09 16:08:16 -0700

    changes little endian

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,29 +3,18 @@
 /**
  * get_endianness - function that checks the endianness.
  *
- * Return: 0 if big endian, 1 if little endian
+ *
+ * Returns: 0 if big endian, 1 if little endian
  **/
 
-int get_endianness(void)
+int get_endianness(void) {
+    unsigned int num = 1;
+    char* ptr = (char*)&num;
 
-{
-	unsigned int num = 1;
-	char* byte = (char *)&num;
-
-	return ((int)*byte);
+    if (*ptr == 1) {
+        return (1);
+    } else {
+        return (0);
+    }
 }
-int main()
-{
-	int endianness = get_endianness();
 
-	if (endianness == 0)
-	{
-		printf("high  Endian\n");
-	}
-	else
-	{printf("lower Endian\n");
-	}
-
-	return (0);
-
-}
